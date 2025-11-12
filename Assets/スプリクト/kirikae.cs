0cc4b08e9db2228374612fc5e612f430156e1e8b
KIC-GAME-C65\student <kof2464711@stu.o-hara.ac.jp>
2025-11-12 12:17:39 +0900

    kirikae

---
+++ Assets/スプリクト/kirikae.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Kirikae : MonoBehaviour
+{
+    public void Kirikae_button()
+    {
+        SceneManager.LoadScene("HomeScene");
+    }
+}
