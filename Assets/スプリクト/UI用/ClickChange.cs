ead6717704a7df0c286294aafa520b7f80417e32
nya <kof2464726@stu.o-hara.ac.jp>
2026-01-14 09:45:11 +0900

    やまさん寝たりする

--- Assets/スプリクト/UI用/ClickChange.cs
+++ Assets/スプリクト/UI用/ClickChange.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -6,11 +6,13 @@ using UnityEngine.EventSystems;
 public class ClickChange : MonoBehaviour, IPointerClickHandler
 {
     public Expression expression;
-    public Sprite expressionSprite;   // 押した時に出す表情
-    public float duration = 3f;
+    //public Sprite expressionSprite;   // 押した時に出す表情
+    //public Sprite duringTempSprite;      // 一時表情中クリック
+    //public float duration = 3f;
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        expression.ShowTemporary(expressionSprite, duration);
+        expression.ShowFoodExpression();
     }
 }
+*/
--- Assets/スプリクト/UI用/Expression.cs
+++ Assets/スプリクト/UI用/Expression.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,29 +7,114 @@ using UnityEngine.EventSystems;
 public class Expression : MonoBehaviour
 {
     public Image statusImage;        // 表示用Image
-    public Sprite normalSprite;      // 通常画像
+    public GameObject gifObject;   // AnimatedImage が付いたオブジェクト
 
-    //public Sprite berrySprite; //イチゴ
-    //public Sprite peachSprite; //ヤマモモ
-    //public Sprite bananaSprite; //バナナ
+    public Sprite normalSprite;      // 通常画像
+    public Sprite idleSprite;        // 放置時画像
+
+    [Header("クリック表情")]
+    public Sprite foodSprite;       // 通常時クリック
+    public Sprite reactSprite;   // 一時表情中クリック
+
+    public float expressionDuration = 3f;
+    public float idleTime = 30f;     // 放置と判定する秒数
 
     Coroutine currentCoroutine;
 
-    public void ShowTemporary(Sprite sprite, float duration)
+    float idleTimer = 0f;
+    bool isIdle = false;
+
+    void Update()
+    {
+        // 何かしら操作があったら
+        if (Input.anyKeyDown || Input.GetMouseButtonDown(0))
+        {
+            idleTimer = 0f;
+
+            if (isIdle)
+            {
+                statusImage.sprite = normalSprite;
+                isIdle = false;
+            }
+        }
+        else
+        {
+            idleTimer += Time.deltaTime;
+
+            if (!isIdle && idleTimer >= idleTime)
+            {
+                // 放置状態へ
+                StopExpression();
+                statusImage.sprite = idleSprite;
+                isIdle = true;
+            }
+        }
+    }
+
+    // ===== 餌クリック用 =====
+    public void ShowFoodExpression()
+    {
+        //if (isIdle) isIdle = false;
+
+        StartExpression(expressionSprite, true);
+    }
+
+    // ===== キャラクリック =====
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (isIdle) return; // 放置中は無視したい場合
+
+        if (IsTemporaryPlaying())
+        {
+            // GIFなし
+            ShowTemporary(reactSprite, duringTempClickSprite, false);
+        }
+    }
+
+    bool IsTemporaryPlaying()
+    {
+        return currentCoroutine != null;
+    }
+
+    public void ShowTemporary(Sprite sprite, float duration, bool showGif)
+    {
+        if (currentCoroutine != null)
+            StopCoroutine(currentCoroutine);
+
+        currentCoroutine = StartCoroutine(ChangeImage(sprite, duration, showGif));
+    }
+
+
+    IEnumerator ChangeImage(Sprite sprite, bool showGif)
+    {
+        statusImage.sprite = sprite;
+
+        //gif表示
+        if (gifObject != null)
+            gifObject.SetActive(true);
+
+        yield return new WaitForSeconds(expressionDuration);
+
+        //元に戻す
+        if (!isIdle)
+            statusImage.sprite = normalSprite;
+
+        if (gifObject != null)
+            gifObject.SetActive(false);
+
+        currentCoroutine = null;
+    }
+
+    void StopExpression()
     {
         if (currentCoroutine != null)
         {
             StopCoroutine(currentCoroutine);
+            currentCoroutine = null;
         }
 
-        currentCoroutine = StartCoroutine(ChangeImage(sprite, duration));
-    }
-
-    IEnumerator ChangeImage(Sprite sprite, float duration)
-    {
-        statusImage.sprite = sprite;
-        yield return new WaitForSeconds(duration);
-        statusImage.sprite = normalSprite;
-        currentCoroutine = null;
+        if (gifObject != null)
+            gifObject.SetActive(false);
     }
 }
+*/
