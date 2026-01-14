07e92d4e11067e71dc6a9643e7d8605262112bf5
nya <kof2464726@stu.o-hara.ac.jp>
2026-01-14 11:42:15 +0900

    げきおこぷんぷんまるになるー

--- Assets/スプリクト/UI用/ClickChange.cs
+++ Assets/スプリクト/UI用/ClickChange.cs
@@ -1,4 +1,4 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -6,13 +6,10 @@ using UnityEngine.EventSystems;
 public class ClickChange : MonoBehaviour, IPointerClickHandler
 {
     public Expression expression;
-    //public Sprite expressionSprite;   // 押した時に出す表情
-    //public Sprite duringTempSprite;      // 一時表情中クリック
-    //public float duration = 3f;
+    public int foodIndex;
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        expression.ShowFoodExpression();
+        expression.ShowFoodExpression(foodIndex);
     }
 }
-*/
---
+++ Assets/スプリクト/UI用/FoodData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class FoodData
+{
+    public Sprite expressionSprite;   // •\î
+    public GameObject gifObject;       // ‘Î‰GIF
+}
+
